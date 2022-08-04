5e1847f947b49ef5e0461ab0b13ced687e08ab87
Nano Nano <neetbenano@gmail.com>
2022-08-04 16:00:00 +0900

    release number fix

--- dist/covid19.js
+++ dist/covid19.js
@@ -1,6 +1,6 @@
 /* covid19 easy checker (ja) by nano (C)NEET.co.,ltd */
 /* License GPL3 https://www.gnu.org/licenses/gpl-3.0.ja.html */
-/* Release 220803-r8 */
+/* Release 220804-r1 */
 /* Kanjicode = UTF-8 */
 /* 1TAB=4Space */
 
